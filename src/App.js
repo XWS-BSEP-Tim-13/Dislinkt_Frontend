d8a1b4d65cdffdc2afdc8775545993039b721c0c
StefanLjubovic <ljubovicstefan@gmail.com>
2022-05-15 14:25:03 +0200

    feature: network page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 //import { useEffect } from 'react';
 //import axios from 'axios';
 import { Route, Routes } from 'react-router-dom';
-
+import { useLocation } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage';
 import Mainpage from './pages/Mainpage/Mainpage';
-
+import Network from './pages/Network/Network';
 import './App.css';
 import Navigation from "../src/components/Navigation/Navigation";
 
@@ -16,13 +16,15 @@ function App() {
         console.log(res.data)
     })
   }, []);*/
+  const location = useLocation();
 
   return (
     <div className="App">
-        <Navigation></Navigation>
+          { location.pathname !== '/' ? <Navigation /> : null }
         <Routes>
             <Route path='/' element={<Mainpage />} />
             <Route path='/home' element={<Homepage />} />
+            <Route path='/network' element={<Network />} />
         </Routes>
     </div>
   );
