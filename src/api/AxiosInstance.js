51f119aaff4705dbafa5374d9f9de71d3c1bb4f4
Ana Gavrilovic <gavrilovic.ra65.2018@uns.ac.rs>
2022-05-23 21:44:57 +0200

    feat: registration request

--- src/api/AxiosInstance.js
+++ src/api/AxiosInstance.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
 
-export const axiosInstance = axios.create({ baseURL: 'http://localhost:8083/' });
+export const axiosInstance = axios.create({ baseURL: 'https://localhost:8083/' });
